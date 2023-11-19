4b51f778b3899edb5ea1f288a375a80e6efe92cf
amirah <mirez.data@gmail.com>
2023-11-19 23:10:32 +0800

    first commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ function Navbar(){
   return(
     <div className="header">
       <nav className="nav-bar">
-        <img src="logo192.png" alt="react-logo" className="nav--logo" />
+        <img src="react-logo.png" alt="react-logo" className="nav--logo" />
         <h2 className="nav--logo_text">ReactFacts</h2>
         <h3 className="nav--title">React Course - Project 1</h3>
       </nav>
